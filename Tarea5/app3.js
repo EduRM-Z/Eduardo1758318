11034e4b0c5a12aafe4a53a5c89b13ad7186a5f6
EduRM-Z <142538701+EduRM-Z@users.noreply.github.com>
2023-09-07 23:09:27 -0600

    Add files via upload
    Tarea 5: Apps de JavaScript

---
+++ Tarea5/app3.js
@@ -0,0 +1,24 @@
+var nombre = "Eduardo";
+var nombre = "Aaron"
+console.log(nombre);
+
+//Comentarios
+let user = "MuroMarron";
+console.log(user);
+
+let numero1 = 2;
+let numero2 = 2;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let mult = numero1 * numero2;
+let div = numero1 / numero2;
+
+console.log("----------------------------")
+
+console.log("suma = " + suma);
+console.log("resta = " + resta);
+console.log("Multipliacion = " + mult);
+console.log("Division = "+div)
+
+
---
+++ Tarea5/app4.js
@@ -0,0 +1,16 @@
+
+let nombreUser = "Juan";
+let edad = 24;
+let nacionalidad = "Mexico";
+let comprometido = "si";
+
+console.log("Nombre es: ");
+console.log(nombreUser);
+console.log("Edad es: ")
+console.log(edad);
+console.log("Su nacionalidad es: ");
+console.log(nacionalidad);
+console.log("Estas comprometid@? ");
+console.log(comprometido);
+
+
